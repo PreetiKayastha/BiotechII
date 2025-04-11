17f70f8bb2937c7840241eb26c204e742614749c
PreetiKayastha <pkayastha@wvstateu.edu>
2025-04-11 15:57:12 -0400

    Add files via upload

---
+++ interesting.py
@@ -0,0 +1,17 @@
+"""
+Interesting.py
+Purpose: This program demonstrates the use of listing.
+Author: Preeti
+Date: April 11, 2025
+"""
+
+#create a list
+family = ['kebal', 'sumitra', 'shristi']
+print (family)
+
+#print specific member of the list
+print(family[2])
+
+#add a new member to the list
+family.append('Ashish')
+print('family after adding Ashish', family)
